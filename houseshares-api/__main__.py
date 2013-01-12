5285773896a7621badb14ac44688a96570a2e65e
Maxwell Harlow <maxharlow@gmail.com>
2013-01-12 14:40:41 +0000

    Run on port specified by environment, if there is one (for Heroku...)

--- houseshares-api/__main__.py
+++ houseshares-api/__main__.py
@@ -1,8 +1,9 @@
 # coding=utf-8
 
+import os
 from api import app
 
 if __name__ == '__main__':
-	app.debug = True
-	app.run(host='0.0.0.0')
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
 
